59227a115528764fa3301fe9e9b6f766f2a8461f
AMAN KUMAR SINGH <amanksingh007@gmail.com>
2018-03-16 00:41:55 +0530

    Update login.php

--- BackendCodes/login.php
+++ BackendCodes/login.php
@@ -1,1 +1,24 @@
-
+<?php
+       
+        $con = mysqli_connect("###HOST###","###USER###","###PASSWORD###","###DATABASE###") or die('Unable to Connect');
+        $userid = $_GET['userid'];
+	
+	    $password = $_GET['password'];
+		if($userid == '' || $password == '' )
+		{
+	
+		echo 'ERROR';
+		}
+		else{
+		$sql = "SELECT * FROM users WHERE userid='$userid' AND password='$password'";
+	        $check = mysqli_fetch_array(mysqli_query($con,$sql));
+		if(isset($check)){
+		echo 'FOUND';
+		}
+		else{ 
+			echo 'NOT_FOUND';
+		 
+        }
+	        
+	   }		
+		?>
