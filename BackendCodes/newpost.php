20b2f56b25215df573bc5d9f0883c222bf69c970
AMAN KUMAR SINGH <amanksingh007@gmail.com>
2018-03-16 00:46:29 +0530

    Update newpost.php

--- BackendCodes/newpost.php
+++ BackendCodes/newpost.php
@@ -1,1 +1,20 @@
 
+<?php
+        $con = mysqli_connect("HOST","id26064","12345678901224","DATABASE") or die('Unable to Connect');
+        $userid = $_GET['userid'];
+	    $password = $_GET['password'];
+	    $post=$_GET['post'];
+	    $post=str_replace("^"," ",$post);
+		if($userid == '' || $password == '' )
+		{
+	
+		echo 'ERROR';
+		}
+		else{
+		$sql = "INSERT INTO POSTS (userid,post) values('$userid','$post');";
+	    if ($con->query($sql) === TRUE)
+             echo "New record created successfully";
+        else
+            echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+		?>
