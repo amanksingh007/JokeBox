e4434510ce76a3951a016ac40b02ef030a9e7911
AMAN KUMAR SINGH <amanksingh007@gmail.com>
2018-03-16 00:45:01 +0530

    Update retrive.php

--- BackendCodes/retrive.php
+++ BackendCodes/retrive.php
@@ -1,1 +1,21 @@
+<?php
 
+$conn = new mysqli($servername, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$ALL_POSTS = array(); 
+$sql = "SELECT userid,post FROM POSTS order by posttime desc;";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$stmt->bind_result($id, $post);
+while($stmt->fetch()){
+	$temp = [
+		'id'=>$id,
+		'post'=>$post
+	];
+	array_push($ALL_POSTS, $temp);
+}
+
+echo json_encode($ALL_POSTS);
+?>
